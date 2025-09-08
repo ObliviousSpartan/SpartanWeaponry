c2e3049da3baaaf91782069b2bab59ac58d66589
JustRed23 <manuel.bormans@gmail.com>
2025-09-08 10:35:17 +0200

    Use entity's random to generate weapons

--- src/main/java/com/oblivioussp/spartanweaponry/util/ItemRandomizer.java
+++ src/main/java/com/oblivioussp/spartanweaponry/util/ItemRandomizer.java
@@ -3,15 +3,15 @@ package com.oblivioussp.spartanweaponry.util;
 import java.util.List;
 
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 
 public class ItemRandomizer 
 {
-	public static ItemStack generate(Level level, List<Item> items)
+	public static ItemStack generate(RandomSource random, List<Item> items)
 	{
-		float weaponRand = level.random.nextFloat();
+		float weaponRand = random.nextFloat();
 		float divider = 1.0f / items.size();
 		int idx = Mth.floor(weaponRand / divider);
 		idx = idx > items.size() - 1 ? items.size() - 1 : idx;
